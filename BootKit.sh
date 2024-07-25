8dff18167514c50cae44df211bd9060d0b388897
allogic <mial.burg@gmail.com>
2024-07-25 11:47:20 -0700

    Working dev environment

--- BootKit.sh
+++ BootKit.sh
@@ -1,157 +1,104 @@
 #!/bin/bash
 
-cmd=$1
+qemu_dir="/mnt/c/Program Files/qemu"
+build_dir="/mnt/c/edk2/Build"
+
+loop_device="/dev/loop4"
+mount_point="/mnt/drive"
+
+qemu_bios_filename="BIOS.BIN"
+loader_efi_filename="BOOTX64.EFI"
+drive_img_filename="DRIVE.IMG"
+
+bios_fd_file="$build_dir/OvmfX64/RELEASE_VS2019/FV/OVMF.fd"
+loader_efi_file="$build_dir/MdeModule/RELEASE_VS2019/X64/Loader.efi"
 
 mute()
 {
   "$@" > /dev/null 2>&1
 }
 
-while [[ $# -gt 0 ]]; do
-	case $1 in
-		--bios-file)
-			if [[ -n "$2" && "$2" != --* ]]; then
-				bios_file="$2"
-				shift
-			fi
-			;;
-		--drive-file)
-			if [[ -n "$2" && "$2" != --* ]]; then
-				drive_file="$2"
-				shift
-			fi
-			;;
-		--loop-dev)
-			if [[ -n "$2" && "$2" != --* ]]; then
-				loop_dev="$2"
-				shift
-			fi
-			;;
-		--mnt-point)
-			if [[ -n "$2" && "$2" != --* ]]; then
-				mnt_point="$2"
-				shift
-			fi
-			;;
-		*)
-		;;
-	esac
-  	shift
-done
+if [[ "$1" == "setup" ]]; then
 
-if [[ "$cmd" == "mkdrive" ]]; then
+	mute git clone https://github.com/tianocore/edk2
+	mute git submodule update --init
+	mute git checkout tags/edk2-stable202405
 
-	if [[ -z "$drive_file" ]]; then
-		echo "Missing drive file"
-		exit 1
-	fi
+	echo "Done"
+fi
 
-	if [[ -z "$loop_dev" ]]; then
-		echo "Missing loopback device"
-		exit 1
-	fi
+if [[ "$1" == "mkdrive" ]]; then
 
-	mute "/mnt/c/Program Files/qemu/qemu-img.exe" create -f raw "$drive_file" 10G
-	mute losetup -P "$loop_dev" "$drive_file"
-	mute parted -s "$loop_dev" mklabel gpt
-	mute parted -s "$loop_dev" mkpart primary fat32 1MiB 100%
-	mute mkfs.msdos -F 32 "$loop_dev"p1
-	mute losetup -d "$loop_dev"
+	mute "$qemu_dir/qemu-img.exe" create -f raw "$drive_img_filename" 10G
+	mute losetup -P "$loop_device" "$drive_img_filename"
+	mute parted -s "$loop_device" mklabel gpt
+	mute parted -s "$loop_device" mkpart primary fat32 1MiB 100%
+	mute mkfs.msdos -F 32 "$loop_device"p1
+	mute losetup -d "$loop_device"
 
 	echo "Done"
 
 fi
 
-if [[ "$cmd" == "mount" ]]; then
+if [[ "$1" == "mount" ]]; then
 
-	if [[ -z "$drive_file" ]]; then
-		echo "Missing drive file"
-		exit 1
-	fi
-
-	if [[ -z "$loop_dev" ]]; then
-		echo "Missing loopback device"
-		exit 1
-	fi
-
-	if [[ -z "$mnt_point" ]]; then
-		echo "Missing mount point"
-		exit 1
-	fi
-
-	mute losetup -P "$loop_dev" "$drive_file"
-	mute mount "$loop_dev"p1 "$mnt_point"
+	mute losetup -P "$loop_device" "$drive_img_filename"
+	mute mount "$loop_device"p1 "$mount_point"
 
 	echo "Done"
 
 fi
 
-if [[ "$cmd" == "umount" ]]; then
+if [[ "$1" == "umount" ]]; then
 
-	if [[ -z "$loop_dev" ]]; then
-		echo "Missing loopback device"
+	if [[ -z "$loop_device" ]]; then
+		echo "Missing --loop-dev"
 		exit 1
 	fi
 
-	if [[ -z "$mnt_point" ]]; then
-		echo "Missing mount point"
-		exit 1
-	fi
-
-	mute umount "$mnt_point"
-	mute losetup -d "$loop_dev"
+	mute umount "$mount_point"
+	mute losetup -d "$loop_device"
 
 	echo "Done"
 
 fi
 
-if [[ "$cmd" == "boot" ]]; then
+if [[ "$1" == "update" ]]; then
 
-	if [[ -z "$bios_file" ]]; then
-		echo "Missing bios file"
-		exit 1
-	fi
+	mute mkdir -p "$mount_point/EFI/BOOT"
+	mute cp "$bios_fd_file" "$qemu_bios_filename"
+	mute cp "$loader_efi_file" "$loader_efi_filename"
+	mute cp "$loader_efi_file" "$mount_point/EFI/BOOT/$loader_efi_filename"
 
-	if [[ -z "$drive_file" ]]; then
-		echo "Missing drive file"
-		exit 1
-	fi
+	echo "Done"
 
-	mute "/mnt/c/Program Files/qemu/qemu-system-x86_64.exe" \
-		-drive file="$drive_file",format=raw,if=virtio \
+fi
+
+if [[ "$1" == "boot" ]]; then
+
+	mute "$qemu_dir/qemu-system-x86_64.exe" \
+		-drive file="$drive_img_filename",format=raw,if=virtio \
 		-m 2G \
-		-bios "$bios_file" \
-		-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
-		-net none
+		-bios "$qemu_bios_filename" \
+		-netdev user,id=net0 -device virtio-net-pci,netdev=net0
 
 	echo "Done"
 
 fi
 
-if [[ "$cmd" == "help" ]] || [[ "$cmd" == "h" ]]; then
+if [[ "$1" == "help" ]]; then
 	echo
 	echo "Usage:"
 	echo
-	echo " BootKit [command] [options]"
+	echo " BootKit [command]"
 	echo
 	echo "Commands:"
-	echo " mkdrive             Make a hard drive image"
-	echo " mount               Mount the hard drive image"
-	echo " umount              Unmount the hard drive image"
-	echo " boot                Boot QEMU for development"
-	echo " help                Display this help message"
-	echo
-	echo "Options:"
-	echo " --bios-file         Specify the "OVMF.fd" BIOS file for QEMU"
-	echo " --drive-file        Raw hard drive image for QEMU to boot from"
-	echo " --loop-dev          Loopback device for the hard drive image"
-	echo " --mnt-point         Mount point for the hard drive image"
-	echo
-	echo "Examples:"
-	echo
-	echo " BootKit mkdrive --drive-file drive.img --loop-dev /dev/loopX"
-	echo " BootKit mount --drive-file drive.img --loop-dev /dev/loopX --mnt-point /mnt/drive"
-	echo " BootKit umount --loop-dev /dev/loopX --mnt-point /mnt/drive"
-	echo " BootKit boot --bios-file OVMF.fd --drive-file drive.img"
+	echo " setup          Setup EDK2 repository"
+	echo " mkdrive        Make a hard drive image"
+	echo " mount          Mount the hard drive image"
+	echo " umount         Unmount the hard drive image"
+	echo " update         Update BIOS and loader EFI file"
+	echo " boot           Boot QEMU"
+	echo " help           Display this help message"
 	echo
 fi
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # BootKit
+
+	EDK2 Setup:
+	------------------------------------------------------------
+
+	git clone https://github.com/tianocore/edk2
+	git submodule update --init
+	git checkout tags/edk2-stable202405
+	Launch Developer Command Prompt for VS20XX
+	edksetup.bat
+	Build BaseTools
+		cd BaseTools
+		nmake
+	Build OVMF BIOS
+		Configure Conf\target.txt
+			ACTIVE_PLATFORM = OvmfPkg/OvmfPkgX64.dsc
+			TARGET_ARCH = X64
+			TOOL_CHAIN_TAG = VS20XX
+		edksetup.bat
+		build
+	Build Custom My Custom Package
+		Configure Conf\target.txt
+			ACTIVE_PLATFORM = MyModulePkg/MyModulePkg.dsc
+			TARGET_ARCH = X64
+			TOOL_CHAIN_TAG = VS20XX
+		edksetup.bat
+		build
+
+	Create Filesystem:
+	------------------------------------------------------------
+
+	qemu-img create -f raw drive.img 10G
+	losetup -f -P drive.img
+	parted /dev/loopX mklabel gpt
+	parted /dev/loopX mkpart primary fat32 1MiB 100%
+	mkfs.msdos -F 32 -f /dev/loopXp1
+	mount /dev/loopXp1 /mnt
+	mkdir -p /mnt/EFI/BOOT
+	cp MyModule.efi /mnt/EFI/BOOT/BOOTX64.EFI
+
+	Launch QEMU:
+	------------------------------------------------------------
+
+	qemu-system-x86_64 `
+		-drive file=drive.img,format=raw,if=virtio `
+		-m 2G `
+		-bios OVMF.fd `
+		-netdev user,id=net0 -device virtio-net-pci,netdev=net0 `
+		-net none
